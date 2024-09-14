1123b9672073fe37cc789bcc1ab93e866c761f20
Aleksandr  Steblevets <fortibrinelike@gmail.com>
2024-09-14 10:49:12 +0300

    @before annotation

--- src/test/java/me/fortibrine/junit/test/PlayerStatisticsTest.java
+++ src/test/java/me/fortibrine/junit/test/PlayerStatisticsTest.java
@@ -2,48 +2,62 @@ package me.fortibrine.junit.test;
 
 import me.fortibrine.junit.Player;
 import me.fortibrine.junit.PlayerStatistics;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 public class PlayerStatisticsTest {
 
+    private Player playerPatrickUnderThirty;
+    private PlayerStatistics statisticsOfPatrickUnderThirty;
+
+    @BeforeEach
+    public void setup() {
+        playerPatrickUnderThirty = new Player("Patrick", 27);
+        statisticsOfPatrickUnderThirty = new PlayerStatistics(playerPatrickUnderThirty, 3, 3);
+    }
+
     @Test
     public void playerNameEqual() {
-        Player player1 = new Player("Patrick", 27);
+
+        System.out.println("Test 1");
+
         Player player2 = new Player("Patrick", 25);
 
-        assertEquals(player1, player2, "Players not equal with same name");
+        assertEquals(playerPatrickUnderThirty, player2, "Players not equal with same name");
 
     }
 
     @Test
     public void playerNameNotEqual() {
-        Player player1 = new Player("Patrick", 27);
+        System.out.println("Test 2");
         Player player2 = new Player("Kalvin", 25);
 
-        assertNotEquals(player1, player2, "Players not equal with same name");
+        assertNotEquals(playerPatrickUnderThirty, player2, "Players not equal with same name");
     }
 
     @Test
     public void youngerPlayerSame() {
-        Player player1 = new Player("Patrick", 27);
+        System.out.println("Test 3");
         Player player2 = new Player("Patrick", 25);
 
         // checks in memory
-        assertSame(player2, PlayerStatistics.getYoungerPlayer(player1, player2), "Younger player not same");
+        assertSame(player2, PlayerStatistics.getYoungerPlayer(playerPatrickUnderThirty, player2), "Younger player not same");
     }
 
     @Test
     public void underThirtyTrue() {
-        Player player1 = new Player("Patrick", 27);
-        PlayerStatistics statistics = new PlayerStatistics(player1, 3, 3);
+        System.out.println("Test 4");
+        PlayerStatistics statistics = new PlayerStatistics(playerPatrickUnderThirty, 3, 3);
 
         assertTrue(statistics.underThirty());
     }
 
     @Test
     public void underThirtyFalse() {
+        System.out.println("Test 5");
+
         Player player1 = new Player("Patrick", 37);
         PlayerStatistics statistics = new PlayerStatistics(player1, 3, 3);
 
@@ -52,6 +66,8 @@ public class PlayerStatisticsTest {
 
     @Test
     public void csvReportNull() {
+        System.out.println("Test 6");
+
         Player player1 = new Player("Patrick", 27);
         PlayerStatistics statistics = new PlayerStatistics(player1, 0, 0);
 
@@ -60,16 +76,18 @@ public class PlayerStatisticsTest {
 
     @Test
     public void csvReportNotNull() {
-        Player player1 = new Player("Patrick", 27);
-        PlayerStatistics statistics = new PlayerStatistics(player1, 3, 3);
+        System.out.println("Test 7");
+
+        PlayerStatistics statistics = new PlayerStatistics(playerPatrickUnderThirty, 3, 3);
 
         assertNotNull(statistics.createCsvRecord());
     }
 
     @Test
     public void getCsvStatsRecord() {
-        Player player1 = new Player("Patrick", 27);
-        PlayerStatistics statistics = new PlayerStatistics(player1, 4, 8);
+        System.out.println("Test 8");
+
+        PlayerStatistics statistics = new PlayerStatistics(playerPatrickUnderThirty, 4, 8);
         Double[] expectedArray = {2d, 0.5};
 
         assertArrayEquals(expectedArray, statistics.createCsvRecord());
